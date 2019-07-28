4d5e1ddd671229f442ef17bd532f4562d3bcd969
Akha Manjezi <amanjezi@gmail.com>
2019-07-28 17:51:19 +0200

    added functionality to display saved entries from shared preferences

--- app/src/main/java/com/example/assignment1/MainActivity.java
+++ app/src/main/java/com/example/assignment1/MainActivity.java
@@ -1,19 +1,33 @@
 package com.example.assignment1;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.lang.reflect.Type;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+    ArrayList<DiaryEntry> entryArrayList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +35,17 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        loadData();
+
+        mRecyclerView = findViewById(R.id.recyclerView);
+        mRecyclerView.setHasFixedSize(true);
+        mLayoutManager = new LinearLayoutManager(this);
+        mAdapter = new DiaryEntryAdapter(entryArrayList);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        mRecyclerView.setAdapter(mAdapter);
+
+        ((TextView) findViewById(R.id.NKIAverageValue)).setText(NKIAverage());
+
 
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -53,5 +78,25 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void loadData() {
+        SharedPreferences sharedPreferences = getSharedPreferences("shared preferences", MODE_PRIVATE);
+        Gson gson = new Gson();
+        String json = sharedPreferences.getString("task list", null);
+        Type type = new TypeToken<ArrayList<DiaryEntry>>() {}.getType();
+        entryArrayList = gson.fromJson(json, type);
+
+        if (entryArrayList == null) {
+            entryArrayList = new ArrayList<>();
+        }
+    }
+
+    private String NKIAverage(){
+        double total = 0;
+        for (int i = 0; i < entryArrayList.size(); i++) {
+            total += Double.parseDouble(entryArrayList.get(i).getNKI());
+        }
+        return String.format("%.2f kJ", total/entryArrayList.size());
     }
 }
