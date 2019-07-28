7bd0b2d118e992ca36434c624c4b7a220e255a5c
Akha Manjezi <amanjezi@gmail.com>
2019-07-28 14:46:02 +0200

    changed handling of updating entry's data

--- app/src/main/java/com/example/assignment1/CalculatorActivity.java
+++ app/src/main/java/com/example/assignment1/CalculatorActivity.java
@@ -5,9 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
@@ -15,11 +18,11 @@ import com.google.gson.reflect.TypeToken;
 import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.Date;
-import java.util.Map;
 
 public class CalculatorActivity extends AppCompatActivity {
 
     ArrayList<DiaryEntry> entryArrayList;
+    DiaryEntry newEntry = new DiaryEntry();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,34 +35,79 @@ public class CalculatorActivity extends AppCompatActivity {
         buttonSave.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                newEntry();
                 saveData();
                 Intent viewEntry = new Intent(getApplicationContext(), EntryActivity.class);
 
                 startActivity(viewEntry);
             }
         });
+
+        TextWatcher textWatcher = new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        };
+
+        EditText breakfastInput = findViewById(R.id.breakfastTextInput);
+        EditText lunchInput = findViewById(R.id.lunchTextInput);
+        EditText dinnerInput = findViewById(R.id.dinnerTextInput);
+        EditText snacksInput = findViewById(R.id.snacksTextInput);
+        EditText weightliftingInput = findViewById(R.id.weightliftingTextInput);
+        EditText cardioInput = findViewById(R.id.cardioTextInput);
+        EditText mixedInput = findViewById(R.id.mixedTextInput);
+        breakfastInput.addTextChangedListener(textWatcher);
+        lunchInput.addTextChangedListener(textWatcher);
+        dinnerInput.addTextChangedListener(textWatcher);
+        snacksInput.addTextChangedListener(textWatcher);
+        weightliftingInput.addTextChangedListener(textWatcher);
+        cardioInput.addTextChangedListener(textWatcher);
+        mixedInput.addTextChangedListener(textWatcher);
+
+
     }
 
-    private void newEntry(){
+    private void updateEntry(){
         EditText breakfastText = findViewById(R.id.breakfastTextInput);
         String breafastKJ = (breakfastText.getText().length() != 0) ? breakfastText.getText().toString() : 0 + "";
+        newEntry.setBreakfast(breafastKJ);
         EditText lunchText = findViewById(R.id.lunchTextInput);
         String lunchKJ = (lunchText.getText().length() != 0) ? lunchText.getText().toString() : 0 + "";
+        newEntry.setLunch(lunchKJ);
         EditText dinnerText = findViewById(R.id.dinnerTextInput);
         String dinnerKJ = (dinnerText.getText().length() != 0) ? dinnerText.getText().toString() : 0 + "";
+        newEntry.setDinner(dinnerKJ);
         EditText snacksText = findViewById(R.id.snacksTextInput);
         String snacksKJ = (snacksText.getText().length() != 0) ? snacksText.getText().toString() : 0 + "";
+        newEntry.setSnacks(snacksKJ);
         EditText weightliftingText = findViewById(R.id.weightliftingTextInput);
         String weightliftingKJ = (weightliftingText.getText().length() != 0) ? weightliftingText.getText().toString() : 0 + "";
+        newEntry.setWeightlifting(weightliftingKJ);
         EditText cardioText = findViewById(R.id.cardioTextInput);
         String cardioKJ = (cardioText.getText().length() != 0) ? cardioText.getText().toString() : 0 + "";
+        newEntry.setCardio(cardioKJ);
         EditText mixedText = findViewById(R.id.mixedTextInput);
         String mixedKJ = (mixedText.getText().length() != 0) ? mixedText.getText().toString() : 0 + "";
-        Date pubDate = new Date();
+        newEntry.setMixed(mixedKJ);
 
-        DiaryEntry newEntry  = new DiaryEntry(breafastKJ, lunchKJ, dinnerKJ, snacksKJ, weightliftingKJ, cardioKJ, mixedKJ, pubDate);
-        entryArrayList.add(newEntry);
+    }
+
+    private void updateTotals(){
+        ((TextView) findViewById(R.id.foodTotalValueView)).setText(newEntry.getFoodKJ());
+        ((TextView) findViewById(R.id.exerciseTotalValueView)).setText(newEntry.getExerciseKJ());
+        ((TextView) findViewById(R.id.foodTotalNKIView)).setText(newEntry.getFoodKJ());
+        ((TextView) findViewById(R.id.exerciseTotalNKIView)).setText(newEntry.getExerciseKJ());
+        ((TextView) findViewById(R.id.NKITotalView)).setText(newEntry.getNKI());
 
     }
 
