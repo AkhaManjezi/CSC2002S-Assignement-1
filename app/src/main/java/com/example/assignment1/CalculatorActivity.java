582b8b9807b7726f1a5de30aad9008dc69a5b7c5
Akha Manjezi <amanjezi@gmail.com>
2019-07-28 12:54:41 +0200

    changed diary entry to store kJ data as strings instead of double

--- app/src/main/java/com/example/assignment1/CalculatorActivity.java
+++ app/src/main/java/com/example/assignment1/CalculatorActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class CalculatorActivity extends AppCompatActivity {
 
@@ -14,6 +15,21 @@ public class CalculatorActivity extends AppCompatActivity {
     }
 
     public void newEntry(View view){
+        EditText breakfastText = findViewById(R.id.breakfastTextInput);
+        double breafastKJ = (breakfastText.getText().length() != 0) ? Double.parseDouble(breakfastText.getText().toString()) : 0;
+        EditText lunchText = findViewById(R.id.breakfastTextInput);
+        double lunchKJ = Double.parseDouble(lunchText.getText().toString());
+        EditText dinnerText = findViewById(R.id.breakfastTextInput);
+        double dinnerKJ = Double.parseDouble(dinnerText.getText().toString());
+        EditText snacksText = findViewById(R.id.breakfastTextInput);
+        double snacksKJ = Double.parseDouble(snacksText.getText().toString());
+        EditText weightliftingText = findViewById(R.id.breakfastTextInput);
+        double weightliftingKJ = Double.parseDouble(weightliftingText.getText().toString());
+        EditText cardioText = findViewById(R.id.breakfastTextInput);
+        double cardioKJ = Double.parseDouble(cardioText.getText().toString());
+        EditText mixedText = findViewById(R.id.breakfastTextInput);
+        double mixedKJ = Double.parseDouble(mixedText.getText().toString());
+
 
     }
 
