35606c606565ed953a67662b00ca2fd71e66e4cc
Akha Manjezi <amanjezi@gmail.com>
2019-07-28 17:50:31 +0200

    Created adapter for cards in recycler view

---
+++ app/src/main/java/com/example/assignment1/DiaryEntryAdapter.java
@@ -0,0 +1,54 @@
+package com.example.assignment1;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+
+public class DiaryEntryAdapter extends RecyclerView.Adapter<DiaryEntryAdapter.DiaryEntryViewHolder> {
+    private ArrayList<DiaryEntry> diaryEntries;
+
+    public static class DiaryEntryViewHolder extends RecyclerView.ViewHolder {
+
+        public TextView dateView;
+        public TextView NKIView;
+
+        public DiaryEntryViewHolder(@NonNull View itemView) {
+            super(itemView);
+            dateView = itemView.findViewById(R.id.dateText);
+            NKIView = itemView.findViewById(R.id.NKIValue);
+        }
+    }
+
+    public DiaryEntryAdapter(ArrayList<DiaryEntry> diaryEntryArrayList){
+        diaryEntries = diaryEntryArrayList;
+    }
+
+    @NonNull
+    @Override
+    public DiaryEntryViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.entry_item, parent, false);
+        DiaryEntryViewHolder diaryEntryViewHolder = new DiaryEntryViewHolder(v);
+        return diaryEntryViewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull DiaryEntryViewHolder holder, int position) {
+        SimpleDateFormat formatter = new SimpleDateFormat("EEE d MMM yyyy HH:mm");
+        DiaryEntry currentEntry = diaryEntries.get(position);
+        holder.NKIView.setText(currentEntry.getNKI());
+        holder.dateView.setText(formatter.format(currentEntry.getDate()));
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return diaryEntries.size();
+    }
+}
