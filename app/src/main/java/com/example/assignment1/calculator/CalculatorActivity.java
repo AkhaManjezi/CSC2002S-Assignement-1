94098033cfd08896d2e2a46c7bbfdd627acf49c5
Akha Manjezi <amanjezi@gmail.com>
2019-08-02 15:14:59 +0200

    no change. android studio error.

---
+++ app/src/main/java/com/example/assignment1/calculator/CalculatorActivity.java
@@ -0,0 +1,219 @@
+package com.example.assignment1.calculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.DatePickerDialog;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.example.assignment1.DiaryEntry;
+import com.example.assignment1.R;
+import com.example.assignment1.entry.EntryActivity;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Collections;
+
+public class CalculatorActivity extends AppCompatActivity {
+
+    ArrayList<DiaryEntry> entryArrayList;
+    DiaryEntry newEntry;
+    int currentIndex = -1;
+    EditText breakfastInput;
+    EditText lunchInput;
+    EditText dinnerInput;
+    EditText snacksInput;
+    EditText weightliftingInput;
+    EditText cardioInput;
+    EditText mixedInput;
+    DatePickerDialog.OnDateSetListener mdateSetListener;
+    Calendar currentDate;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calculator);
+
+        loadData();
+
+         breakfastInput = findViewById(R.id.breakfastTextInput);
+         lunchInput = findViewById(R.id.lunchTextInput);
+         dinnerInput = findViewById(R.id.dinnerTextInput);
+         snacksInput = findViewById(R.id.snacksTextInput);
+         weightliftingInput = findViewById(R.id.weightliftingTextInput);
+         cardioInput = findViewById(R.id.cardioTextInput);
+         mixedInput = findViewById(R.id.mixedTextInput);
+
+        final SimpleDateFormat formatter = new SimpleDateFormat("EEE d MMM yyyy");
+
+        currentDate = Calendar.getInstance();
+        final TextView dateText = findViewById(R.id.dateText);
+
+        Button buttonSave = findViewById(R.id.saveButton);
+        buttonSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(currentIndex != -1){
+                    entryArrayList.set(currentIndex,newEntry);
+                }else{
+                    entryArrayList.add(newEntry);
+                }
+                saveData();
+                currentIndex = entryArrayList.indexOf(newEntry);
+                Intent viewEntry = new Intent(getApplicationContext(), EntryActivity.class);
+                viewEntry.putExtra("Uniqid", "From_calculator");
+                viewEntry.putExtra("index", (currentIndex)+"");
+
+                startActivity(viewEntry);
+                finish();
+            }
+        });
+
+        Intent intent = this.getIntent();
+        if(intent.getExtras().getString("Uniqid").equals("From_diary")){
+            currentIndex = Integer.parseInt(intent.getExtras().getString("index"));
+            newEntry = entryArrayList.get(currentIndex);
+            dateText.setText(formatter.format(newEntry.getDate().getTime()));
+            currentDate = newEntry.getDate();
+            edit();
+            updateTotals();
+        }else{
+            newEntry = new DiaryEntry();
+            dateText.setText(formatter.format(currentDate.getTime()));
+        }
+
+        TextWatcher textWatcher = new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                updateEntry();
+                updateTotals();
+
+            }
+        };
+
+        breakfastInput.addTextChangedListener(textWatcher);
+        lunchInput.addTextChangedListener(textWatcher);
+        dinnerInput.addTextChangedListener(textWatcher);
+        snacksInput.addTextChangedListener(textWatcher);
+        weightliftingInput.addTextChangedListener(textWatcher);
+        cardioInput.addTextChangedListener(textWatcher);
+        mixedInput.addTextChangedListener(textWatcher);
+
+        dateText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int year = currentDate.get(Calendar.YEAR);
+                int month = currentDate.get(Calendar.MONTH);
+                int day = currentDate.get(Calendar.DAY_OF_MONTH);
+
+                DatePickerDialog dialog = new DatePickerDialog(CalculatorActivity.this, android.R.style.Theme_Material_Dialog_MinWidth, mdateSetListener, year, month, day);
+//                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(getResources().getColor(android.R.color.transparent)));
+                dialog.show();
+            }
+        });
+
+        mdateSetListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                currentDate = Calendar.getInstance();
+                currentDate.set(Calendar.YEAR,year);
+                currentDate.set(Calendar.MONTH,month);
+                currentDate.set(Calendar.DAY_OF_MONTH, day);
+                newEntry.setDate(currentDate);
+                dateText.setText(formatter.format(currentDate.getTime()));
+            }
+        };
+
+
+    }
+
+    private void updateEntry(){
+        EditText breakfastText = findViewById(R.id.breakfastTextInput);
+        String breafastKJ = (breakfastText.getText().length() != 0) ? breakfastText.getText().toString() : 0 + "";
+        newEntry.setBreakfast(breafastKJ);
+        EditText lunchText = findViewById(R.id.lunchTextInput);
+        String lunchKJ = (lunchText.getText().length() != 0) ? lunchText.getText().toString() : 0 + "";
+        newEntry.setLunch(lunchKJ);
+        EditText dinnerText = findViewById(R.id.dinnerTextInput);
+        String dinnerKJ = (dinnerText.getText().length() != 0) ? dinnerText.getText().toString() : 0 + "";
+        newEntry.setDinner(dinnerKJ);
+        EditText snacksText = findViewById(R.id.snacksTextInput);
+        String snacksKJ = (snacksText.getText().length() != 0) ? snacksText.getText().toString() : 0 + "";
+        newEntry.setSnacks(snacksKJ);
+        EditText weightliftingText = findViewById(R.id.weightliftingTextInput);
+        String weightliftingKJ = (weightliftingText.getText().length() != 0) ? weightliftingText.getText().toString() : 0 + "";
+        newEntry.setWeightlifting(weightliftingKJ);
+        EditText cardioText = findViewById(R.id.cardioTextInput);
+        String cardioKJ = (cardioText.getText().length() != 0) ? cardioText.getText().toString() : 0 + "";
+        newEntry.setCardio(cardioKJ);
+        EditText mixedText = findViewById(R.id.mixedTextInput);
+        String mixedKJ = (mixedText.getText().length() != 0) ? mixedText.getText().toString() : 0 + "";
+        newEntry.setMixed(mixedKJ);
+
+    }
+
+    private void updateTotals(){
+        ((TextView) findViewById(R.id.foodTotalValueView)).setText(newEntry.getFoodKJ());
+        ((TextView) findViewById(R.id.exerciseTotalValueView)).setText(newEntry.getExerciseKJ());
+        ((TextView) findViewById(R.id.foodTotalNKIView)).setText(newEntry.getFoodKJ());
+        ((TextView) findViewById(R.id.exerciseTotalNKIView)).setText(newEntry.getExerciseKJ());
+        ((TextView) findViewById(R.id.NKITotalView)).setText(newEntry.getNKI());
+
+    }
+
+    private void saveData() {
+        SharedPreferences sharedPreferences = getSharedPreferences("shared preferences", MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        Gson gson = new Gson();
+        Collections.sort(entryArrayList);
+        String json = gson.toJson(entryArrayList);
+        editor.putString("task list", json);
+        editor.apply();
+    }
+
+    private void loadData() {
+        SharedPreferences sharedPreferences = getSharedPreferences("shared preferences", MODE_PRIVATE);
+        Gson gson = new Gson();
+        String json = sharedPreferences.getString("task list", null);
+        Type type = new TypeToken<ArrayList<DiaryEntry>>() {}.getType();
+        entryArrayList = gson.fromJson(json, type);
+
+        if (entryArrayList == null) {
+            entryArrayList = new ArrayList<>();
+        }
+    }
+
+    private void edit() {
+
+        breakfastInput.setText(newEntry.getBreakfast());
+        lunchInput.setText(newEntry.getLunch());
+        dinnerInput.setText(newEntry.getDinner());
+        snacksInput.setText(newEntry.getSnacks());
+        weightliftingInput.setText(newEntry.getWeightlifting());
+        cardioInput.setText(newEntry.getCardio());
+        mixedInput.setText(newEntry.getMixed());
+    }
+
+}
---
+++ app/src/main/java/com/example/assignment1/main/MainActivity.java
@@ -0,0 +1,124 @@
+package com.example.assignment1.main;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+
+import com.example.assignment1.calculator.CalculatorActivity;
+import com.example.assignment1.DiaryEntry;
+import com.example.assignment1.DiaryEntryAdapter;
+import com.example.assignment1.R;
+import com.example.assignment1.entry.EntryActivity;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    private RecyclerView mRecyclerView;
+    private DiaryEntryAdapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+    ArrayList<DiaryEntry> entryArrayList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        loadData();
+
+        buildRecyclerView();
+
+        ((TextView) findViewById(R.id.NKIAverageValue)).setText(NKIAverage());
+
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent newCalculation = new Intent(getApplicationContext(), CalculatorActivity.class);
+                newCalculation.putExtra("Uniqid", "From_overview");
+
+                startActivity(newCalculation);
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void loadData() {
+        SharedPreferences sharedPreferences = getSharedPreferences("shared preferences", MODE_PRIVATE);
+        Gson gson = new Gson();
+        String json = sharedPreferences.getString("task list", null);
+        Type type = new TypeToken<ArrayList<DiaryEntry>>() {}.getType();
+        entryArrayList = gson.fromJson(json, type);
+
+        if (entryArrayList == null) {
+            entryArrayList = new ArrayList<>();
+        }
+    }
+
+    private String NKIAverage(){
+        double total = 0;
+        for (int i = 0; i < entryArrayList.size(); i++) {
+            total += Double.parseDouble(entryArrayList.get(i).getNKI());
+        }
+        String output = (entryArrayList.size()==0) ? "0.0 kJ" : String.format("%.2f kJ", total/entryArrayList.size());
+        return output;
+    }
+
+    private void buildRecyclerView(){
+        mRecyclerView = findViewById(R.id.recyclerView);
+        mRecyclerView.setHasFixedSize(true);
+        mLayoutManager = new LinearLayoutManager(this);
+        mAdapter = new DiaryEntryAdapter(entryArrayList);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        mRecyclerView.setAdapter(mAdapter);
+
+        mAdapter.setOnItemClickListener(new DiaryEntryAdapter.OnItemClickListener() {
+            @Override
+            public void onItemClick(int position) {
+                Intent viewEntry = new Intent(getApplicationContext(), EntryActivity.class);
+                viewEntry.putExtra("Uniqid", "From_overview");
+                viewEntry.putExtra("index", (position)+"");
+
+                startActivity(viewEntry);
+                finish();
+            }
+        });
+
+    }
+}
