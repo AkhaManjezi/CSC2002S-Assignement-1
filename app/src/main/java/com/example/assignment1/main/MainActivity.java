8fa8c910529e1fbb25093b17a530dcbdd460d965
Akha Manjezi <amanjezi@gmail.com>
2019-08-03 22:38:26 +0200

    optimised imports

--- app/src/main/java/com/example/assignment1/main/MainActivity.java
+++ app/src/main/java/com/example/assignment1/main/MainActivity.java
@@ -3,8 +3,6 @@ package com.example.assignment1.main;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
